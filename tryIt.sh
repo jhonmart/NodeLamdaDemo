3d79e1a7cc31205f71e415abd954ed7ee1ee3627
Jonatas Martins <jhonararipe@gmail.com>
2023-01-31 09:14:30 -0300

    Starting api gateway integration on aws cli

--- tryIt.sh
+++ tryIt.sh
@@ -10,6 +10,41 @@ aws --endpoint-url=http://localhost:4566 lambda invoke \
   --payload '{"key": "value"}' \
   output.json
 
+aws --endpoint-url=http://localhost:4566 apigateway create-rest-api \
+  --name dev-hello
+
+aws --endpoint-url=http://localhost:4566 apigateway get-resources \
+  --rest-api-id fx73ji8x0e
+
+aws --endpoint-url=http://localhost:4566 apigateway create-resource \
+  --rest-api-id fx73ji8x0e \
+  --parent-id p6khoyblxx \
+  --path-part demo
+
+aws --endpoint-url=http://localhost:4566 apigateway put-method \
+  --rest-api-id fx73ji8x0e \
+  --resource-id q2grh13nyy \
+  --http-method POST \
+  --authorization-type NONE
+
+aws --endpoint-url=http://localhost:4566 apigateway put-integration \
+  --rest-api-id fx73ji8x0e \
+  --resource-id q2grh13nyy \
+  --http-method POST \
+  --type AWS \
+  --integration-http-method POST \
+  --uri arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:*:function:lambda-dev-hello/invocations
+
+aws --endpoint-url=http://localhost:4566 apigateway create-deployment \
+  --rest-api-id fx73ji8x0e \
+  --stage-name prod
+
+aws --endpoint-url=http://localhost:4566 apigateway get-stage \
+  --rest-api-id fx73ji8x0e \
+  --stage-name prod
+
+curl -X POST http://localhost:4566/restapis/fx73ji8x0e/prod/demo
+
 {
     "FunctionName": "lambda-dev-hello",
     "FunctionArn": "arn:aws:lambda:us-east-1:000000000000:function:lambda-dev-hello",
@@ -39,4 +74,54 @@ aws --endpoint-url=http://localhost:4566 lambda invoke \
   "StatusCode": 200,
   "LogResult": "",
   "ExecutedVersion": "$LATEST"
+}
+
+{
+    "id": "fx73ji8x0e",
+    "name": "dev-hello",
+    "createdDate": 1675164010.0,
+    "apiKeySource": "HEADER",
+    "endpointConfiguration": {
+        "types": [
+            "EDGE"
+        ]
+    },
+    "disableExecuteApiEndpoint": false
+}
+
+{
+    "items": [
+        {
+            "id": "p6khoyblxx",
+            "path": "/"
+        }
+    ]
+}
+
+{
+    "id": "q2grh13nyy",
+    "parentId": "p6khoyblxx",
+    "pathPart": "demo",
+    "path": "/demo"
+}
+
+{
+    "httpMethod": "POST",
+    "authorizationType": "NONE",
+    "apiKeyRequired": false,
+    "methodResponses": {}
+}
+
+{
+    "type": "AWS",
+    "httpMethod": "POST",
+    "uri": "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:000000000000:function:lambda-dev-hello/invocations",
+    "requestParameters": {},
+    "cacheNamespace": "q2grh13nyy",
+    "cacheKeyParameters": []
+}
+
+{
+    "id": "fg2qxuuv71",
+    "createdDate": 1675166142.0
 }
